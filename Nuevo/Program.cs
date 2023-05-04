5ae88a2d15813bbeb5c681e69bf56d2ad58f2d56
XITZEL <124205150+XITZEL@users.noreply.github.com>
2023-05-04 19:55:52 +0000

    d

---
+++ Nuevo/Program.cs
@@ -0,0 +1,27 @@
+﻿class MiExcepcion:Exception
+{
+    public MiExcepcion(string message): base(message)//
+    {
+
+    }
+    public MiExcepcion():this("Error")//
+    {
+
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try 
+        {
+                throw new NotImplementedException("Error");
+        }
+        catch(MiExcepcion e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+
+    }
+}
